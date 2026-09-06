8ac36b8ebb14089fea71f25945d6f74db9948bfa
Sk Naif Alam <naifsheikh0808@gmail.com>
2026-09-06 16:41:20 +0530

    Time: 9 ms (89.22%), Space: 9 MB (91.43%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = t.size();
+        const long long LIMIT = INT_MAX;
+
+        vector<long long> dp(n + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i <= s.size(); i++) {
+            for (int j = n; j >= 1; j--) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[j] += dp[j - 1];
+
+                    // Prevent overflow.
+                    if (dp[j] > LIMIT) {
+                        dp[j] = LIMIT;
+                    }
+                }
+            }
+        }
+
+        return (int)dp[n];
+    }
+};
